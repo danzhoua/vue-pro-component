9bd3aa5e3567f939e26e1c23fbb9dc447f059dad
zhoudan <zhoudan@xmov.ai>
2022-10-28 07:38:07 +0800

    version update

--- packages/vue-pro-componentchildren/__tests__/vue-pro-componentchildren.test.js
+++ packages/vue-pro-componentchildren/__tests__/vue-pro-componentchildren.test.js
@@ -1,7 +1,9 @@
-'use strict';
+'use strict'
 
-const vueProComponentchildren = require('..');
-const assert = require('assert').strict;
+const vueProComponentchildren = require('..')
+const assert = require('assert').strict
 
-assert.strictEqual(vueProComponentchildren(), 'Hello from vueProComponentchildren');
-console.info("vueProComponentchildren tests passed");
+assert.strictEqual(vueProComponentchildren(), 'Hello from vueProComponentchildren')
+console.info('vueProComponentchildren tests passed')
+// 测试包版本更新
+console.log('test')
